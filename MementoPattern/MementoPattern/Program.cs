b318313e50ed4cbbc859f70c4e05c7ec0b6c5689
Siddi <cs185304@ncr.com>
2020-03-23 17:44:34 +0530

    first

---
+++ MementoPattern/MementoPattern/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MementoPattern
+{
+    public class Originator
+    {
+        private string state;
+
+        public Memento CreateMemento()
+        {
+            return new Memento(state);
+        }
+
+        public void SetMemento(Memento memento)
+        {
+            this.state = memento.GetState();
+        }
+    }
+
+    public class Memento
+    {
+        private string state;
+
+        public Memento(string state)
+        {
+            this.state = state;
+        }
+
+        public string GetState()
+        {
+            return state;
+        }
+    }
+
+    public class Caretaker
+    {
+        public Memento Memento { get; set; }
+    }
+
+
+}
